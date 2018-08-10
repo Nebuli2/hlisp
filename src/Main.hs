b8a6c3354dffba667a3a7677a552e5f3337cd01f
Benjamin Hetherington <b.w.hetherington@gmail.com>
2018-08-10 00:29:35 -0400

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,78 @@
+module Main where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data Expression
+  = List [Expression]
+  | Number Double
+  | String String
+  | Bool Bool
+  | Atom String
+
+instance Show Expression where
+  show (Number n) = show n
+  show (String s) = "\"" ++ s ++ "\""
+  show (Bool True) = "#t"
+  show (Bool False) = "#f"
+  show (Atom a) = a
+  show (List xs) = "(" ++ (unwords . map show) xs ++ ")"
+
+parseString :: Parser Expression
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser Expression
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    atom -> Atom atom
+
+parseNumber :: Parser Expression
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseListInterior :: Parser Expression
+parseListInterior = liftM List $ sepBy parseExpression spaces
+
+parseQuote :: Parser Expression
+parseQuote = do
+  char '\''
+  x <- parseExpression
+  return $ List [Atom "quote", x]
+
+parseList :: Parser Expression
+parseList = do
+  char '('
+  x <- parseListInterior
+  char ')'
+  return x
+
+parseExpression :: Parser Expression
+parseExpression = parseAtom
+  <|> parseString
+  <|> parseNumber
+  <|> parseQuote
+  <|> parseList
+
+main :: IO ()
+main = do
+  putStrLn "Enter an expression:"
+  line <- getLine
+  let expr = parse parseExpression "lisp" line
+  case expr of
+    Right x -> print x
+    Left err -> putStrLn $ "Error: " ++ show err
